8809a22a3bcad8e11c0370902a4d20cf473d8d96
Kasenhova67 <katya.gusentsova84@gmail.com>
2026-04-26 23:49:41 +0300

    fix: ri

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const reversed = parseInt(
+    Math.abs(n).toString().split('').reverse().join(''),
+    10
+  );
+  return reversed;
 };
